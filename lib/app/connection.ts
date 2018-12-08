5b4fe2831573f3e47f21e9b75cd4704dafee5ded
Lasana Murray <lmurray@quenk.com>
2018-12-08 19:36:43 -0400

    chore: update some comments

--- lib/app/connection.ts
+++ lib/app/connection.ts
@@ -22,27 +22,33 @@ export interface Store {
 }
 
 /**
- * Connection
+ * Connection interface.
+ *
+ * This interface is used to abstract various data stores and
+ * third party services the app may maintain persistenta open connections
+ * to.
  */
 export interface Connection {
 
     /**
      * open the connection.
+     *
+     * When called this method can initialize a new connection/pool or 
+     * simply lie for connections that are not meant to be kept open.
      */
     open(): Future<Connection>;
 
     /**
-     * checkout the actual connection implementation.
+     * checkout the actual underlying implementation.
      *
-     * This is the value will be used by applications
-     * to run queries, send data etc.
+     * This method provides the interface (in an unsafe manner)
+     * to the library that is used to actually interface with the
+     * remote service.
      *
-     * NOTE: We use any here to avoid carrying the type
-     * all the way to the application. That may be the safer
-     * thing to do but is a pain manage.
-     * It looks like with strict:true, typescript won't allow
-     * implementors to override a method type parameter here
-     * that is determined by return value only.
+     * NOTE: We use <any> here to avoid carrying the type
+     * all the way to the App class.
+     *
+     * This was done to ease the burden of using this API.
      */
     checkout(): Future<any>;
 
