1c4ac0331116d9879b172efa5f5f1802586c9ab7
Lasana Murray <lmurray@quenk.com>
2019-01-23 23:53:27 -0400

    refactor(app/api/action): drop wait in favour of await

---
+++ src/app/api/action/await.ts
@@ -0,0 +1,34 @@
+import { liftF } from '@quenk/noni/lib/control/monad/free';
+import { Future, pure } from '@quenk/noni/lib/control/monad/future';
+import { compose, noop, identity } from '@quenk/noni/lib/data/function';
+import { Context } from '../context';
+import { Action, ActionM } from './';
+
+/**
+ * Await action.
+ */
+export class Await<A> extends Action<A>{
+
+    constructor(
+        public f: () => Future<any>,
+        public next: (a: any) => A) { super(next); }
+
+    map<B>(f: (a: A) => B): Await<B> {
+
+        return new Await(this.f, compose(this.next, f));
+
+    }
+
+    exec(ctx: Context<A>): Future<A> {
+
+        return this.f().map(this.next);
+
+    }
+
+}
+
+/**
+ * await a value from an asynchrounous operation before continuing.
+ */
+export const await = <A>(f: () => Future<A>): ActionM<A> =>
+    liftF(new Await(f, identity));
--- test/fixtures/ledger/modules/accounts/handlers.ts
+++ test/fixtures/ledger/modules/accounts/handlers.ts
@@ -1,18 +1,18 @@
 import * as pool from '../../../../../src/app/api/action/pool';
 import { Object } from '@quenk/noni/lib/data/json';
 import { Future, pure } from '@quenk/noni/lib/control/monad/future';
-import { Request} from '../../../../../src/app/api/request';
-import {  ActionM  } from '../../../../../src/app/api/action';
-import {  wait } from '../../../../../src/app/api/action/wait';
+import { Request } from '../../../../../src/app/api/request';
+import { ActionM } from '../../../../../src/app/api/action';
+import { await } from '../../../../../src/app/api/action/await';
 import { created } from '../../../../../src/app/api/action/http/response/created';
 import { Memgo } from '../../../memgodb';
 
 export const create = (r: Request): Future<ActionM<undefined>> =>
-        pure(pool
-            .checkout<Memgo>('main')
-            .chain(m => wait(doCreate('account', r.body, m))));
+    pure(pool
+        .checkout<Memgo>('main')
+        .chain(m => await(() => doCreate('account', r.body, m)))
+        .chain(id => created({ id })));
 
-const doCreate = (name: string, body: Object, m: Memgo): Future<ActionM<undefined>> =>
+const doCreate = (name: string, body: Object, m: Memgo): Future<number> =>
     m.collection(name)
-        .insert(body)
-        .chain(id => pure(created({ id })));
+        .insert(body);
