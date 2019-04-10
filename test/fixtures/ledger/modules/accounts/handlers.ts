33eacc4b156e21ca8327a86f4333aea3b90baa10
Lasana Murray <lmurray@quenk.com>
2019-04-10 06:58:38 -0400

    refactor(app/api/action): add value action

--- test/fixtures/ledger/modules/accounts/handlers.ts
+++ test/fixtures/ledger/modules/accounts/handlers.ts
@@ -1,9 +1,10 @@
 import * as pool from '../../../../../src/app/api/action/pool';
 import { Object } from '@quenk/noni/lib/data/json';
-import { Future  } from '@quenk/noni/lib/control/monad/future';
+import { Future } from '@quenk/noni/lib/control/monad/future';
 import { Request } from '../../../../../src/app/api/request';
 import { ActionM } from '../../../../../src/app/api/action';
 import { await } from '../../../../../src/app/api/action/await';
+import { value } from '../../../../../src/app/api/action/control';
 import { created } from '../../../../../src/app/api/action/response';
 import { Memgo } from '../../../memgodb';
 
@@ -11,7 +12,8 @@ export const create = (r: Request): ActionM<undefined> =>
     (pool
         .checkout<Memgo>('main')
         .chain(m => await(() => doCreate('account', r.body, m)))
-        .chain(id => created({ id })));
+        .chain(id => value(created({ id })))
+        .chain(r => r));
 
 const doCreate = (name: string, body: Object, m: Memgo): Future<number> =>
     m.collection(name)
