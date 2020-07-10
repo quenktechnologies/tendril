590aaf12699392b955301483ea4a6557ab30a0d1
Lasana Murray <lmurray@quenk.com>
2020-07-10 18:43:32 -0400

    refactor: adds session configuration types

---
+++ src/app/middleware/session/store/provider/memory.ts
@@ -0,0 +1,20 @@
+import * as session from 'express-session';
+
+import { Future, pure } from '@quenk/noni/lib/control/monad/future';
+
+import { SessionStoreProvider } from './';
+
+/**
+ * MemoryStoreProvider provides the express-session#MemoryStore Store.
+ *
+ * Note: This should only be used during development.
+ */
+export class MemoryStoreProvider implements SessionStoreProvider {
+
+    create(): Future<session.Store> {
+
+        return pure(<session.Store>new session.MemoryStore());
+
+    }
+
+}
